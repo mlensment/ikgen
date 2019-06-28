1fcbbed5da43f8487a14070ecdce871239296b0e
Sven Erik Rebane <sven.erik@mindly.ee>
2019-06-28 11:35:21 +0300

    identity code will be automatically added to clipboard

--- ik.rb
+++ ik.rb
@@ -1,3 +1,33 @@
+module OS
+  def OS.windows?
+    return /win32/ =~RUBY_PLATFORM
+  end
+  def OS.mac?
+    return /darwin/ =~ RUBY_PLATFORM
+  end
+  def OS.linux?
+    return /linux/ =~ RUBY_PLATFORM
+  end
+end
+
+def ending idcode
+  puts idcode
+  if ["-c", "--clipboard"].include? ARGV[0]
+    if OS.windows?
+      IO.popen('clip', 'w') { |f| f << idcode }
+    elsif OS.mac?
+      IO.popen('pbcopy', 'w') { |f| f << idcode }
+    elsif OS.linux?
+      IO.popen('xsel –clipboard –input', '2+') { |f| f << idcode }
+    else
+      puts "Clipboard not supported on your system"
+      exit
+    end
+    puts "Copied to clipboard"
+    exit
+  end
+end
+
 gender = (3..4).to_a.sample
 year = (50..99).to_a.sample.to_s.rjust(2, '0')
 month = (1..12).to_a.sample.to_s.rjust(2, '0')
@@ -17,8 +47,7 @@ end
 ctrl = (ctrl_sum % 11).floor
 
 if ctrl != 10
-  puts "#{ik}#{ctrl}"
-  exit
+  ending "#{ik}#{ctrl}"
 end
 
 ctrl_sum = 0.0
@@ -29,8 +58,7 @@ end
 ctrl = (ctrl_sum % 11).floor
 
 if ctrl == 10
-  puts "#{ik}0"
-  exit
+  ending "#{ik}0"
 end
 
-puts "#{ik}#{ctrl}"
+ending "#{ik}#{ctrl}"
